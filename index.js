09e1821249b80e4a6cfc81b76cd23a46ed45fc2a
Ryan Rosztoczy <rrosztoczy@gmail.com>
2018-12-09 22:44:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('.target');
+}
+
+function increaseRankBy(n) {
+  var ranks = document.querySelectorAll('.ranked-list li');
+  for (let i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML = (parseInt(ranks[i].innerHTML) + n).toString();
+  }
+}
+
+function deepestChild() {
+  var deepest = document.querySelector('#grand-node');
+  var i;
+  for (i = 0; i < deepest.childNodes.length; i++) {
+    deepest = deepest.childNodes[i];
+  }
+  console.log('hello')
+  return deepest;
+}
